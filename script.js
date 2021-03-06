feb74aa04999bc95fda3721deb6fb311ad85187e
admin <lucie.petrtylova@studentstc.cz>
2021-03-06 19:04:11 +0100

    cm.17

--- script.js
+++ script.js
@@ -5,22 +5,5 @@
     var image2=new Image()
     image2.src="obr2.png"
 
-    function myFunction(imgs) {
-        // Get the expanded image
-        var expandImg = document.getElementById("expandedImg");
-        // Get the image text
-        var imgText = document.getElementById("imgtext");
-        // Use the same src in the expanded image as the image being clicked on from the grid
-        expandImg.src = imgs.src="nahrdelniku.jpg";
-        imgs.src="květiny1u.jpg";
-        imgs.src="nausnice2u.jpg";
-        imgs.src="hrnkyp.jpg";
 
-        // Use the value of the alt attribute of the clickable image as text inside the expanded image
-        imgText.innerHTML = imgs.alt="Snow";imgs.alt="Nature";
-        imgs.alt="Mountains";
-        imgs.alt="light";
-        // Show the container element (hidden with CSS)
-        expandImg.parentElement.style.display = "block";
-    }
 
