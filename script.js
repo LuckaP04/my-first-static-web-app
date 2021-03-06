872dd5a0270a06ab612c4e15b74abfcf37456446
admin <lucie.petrtylova@studentstc.cz>
2021-03-06 17:46:06 +0100

    cm.17

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
         // Get the image text
         var imgText = document.getElementById("imgtext");
         // Use the same src in the expanded image as the image being clicked on from the grid
-        expandImg.src = imgs.src="nahrdelnik.jpg"; imgs.src="květiny1.jpg";
+        expandImg.src = imgs.src="nahrdelniku.jpg"; imgs.src="květiny1u.jpg";
 
         // Use the value of the alt attribute of the clickable image as text inside the expanded image
         imgText.innerHTML = imgs.alt="Snow";imgs.alt="Nature";
