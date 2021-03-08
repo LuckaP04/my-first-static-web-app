91bf0dd3d2fe756a8cf3af733f7f8b66a708ebc6
admin <lucie.petrtylova@studentstc.cz>
2021-03-08 17:02:43 +0100

    cm.19

--- script.js
+++ script.js
@@ -5,5 +5,11 @@
     var image2=new Image()
     image2.src="obr2.png"
 
+    // Get the modal
+    var modal = document.getElementById('id01');
 
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+
+    }
 
